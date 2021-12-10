7e271f39269bfd458c46c4bc58449ce63b1e6757
tirathawat@gmail.com <tirathawat@gmail.com>
2021-12-11 00:26:54 +0700

    feat: add menu and header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,47 @@
 import React from "react";
-import { Button } from "@material-ui/core";
+
+import { Container } from "@material-ui/core";
+
+import Header from "./components/templates/Header";
+import Menu from "./components/templates/Menu";
+
+import Login from "./components/pages/Login";
+import Register from "./components/pages/Register";
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Navigate,
+  Routes,
+} from "react-router-dom";
 
 export default function App() {
+  const [openDrawer, setOpenDrawer] = React.useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpenDrawer(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpenDrawer(false);
+  };
+
   return (
-    <div>
-      <Button>test</Button>
-    </div>
+    <Router>
+      <Header open={openDrawer} handleDrawerOpen={handleDrawerOpen} />
+      <Menu open={openDrawer} handleDrawerClose={handleDrawerClose} />
+      <Container pt={12}>
+        <Routes>
+          <Route
+            path="/"
+            exact={true}
+            component={() => <Navigate to="/login" />}
+          />
+
+          <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
+        </Routes>
+      </Container>
+    </Router>
   );
 }
--- src/components/templates/Header.jsx
+++ src/components/templates/Header.jsx
@@ -1,9 +1,77 @@
-import React from 'react'
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import clsx from "clsx";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
 
-export default function Header() {
-    return (
-        <div>
-            
-        </div>
-    )
+import PropTypes from "prop-types";
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+  appBar: {
+    transition: theme.transitions.create(["margin", "width"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: drawerWidth,
+    transition: theme.transitions.create(["margin", "width"], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+}));
+
+export default function Header(props) {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar
+        position="static"
+        className={clsx(classes.appBar, {
+          [classes.appBarShift]: props.open,
+        })}
+      >
+        <Toolbar>
+          {!props.open && (
+            <IconButton
+              edge="start"
+              className={classes.menuButton}
+              color="inherit"
+              aria-label="menu"
+              onClick={props.handleDrawerOpen}
+            >
+              <MenuIcon />
+            </IconButton>
+          )}
+          <Typography variant="h6" className={classes.title}>
+            News
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
 }
+
+Header.propTypes = {
+  open: PropTypes.bool,
+  handleDrawerOpen: PropTypes.func,
+};
